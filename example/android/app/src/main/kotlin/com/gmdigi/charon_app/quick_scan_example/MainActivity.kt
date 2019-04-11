a724e228839ca17ea8432ba3d1ad3a9a2461ac67
cyan92128505 <cyan92128505@gmail.com>
2019-04-11 10:26:53 +0800

    Use kotlin and swift

---
+++ example/android/app/src/main/kotlin/com/gmdigi/charon_app/quick_scan_example/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.gmdigi.charon_app.quick_scan_example
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
--- ios/Classes/QuickScanPlugin.m
+++ ios/Classes/QuickScanPlugin.m
@@ -1,20 +1,8 @@
 #import "QuickScanPlugin.h"
+#import <quick_scan/quick_scan-Swift.h>
 
 @implementation QuickScanPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"quick_scan"
-            binaryMessenger:[registrar messenger]];
-  QuickScanPlugin* instance = [[QuickScanPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+  [SwiftQuickScanPlugin registerWithRegistrar:registrar];
 }
-
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
-}
-
 @end
